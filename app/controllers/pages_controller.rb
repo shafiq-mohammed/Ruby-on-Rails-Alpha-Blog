776b1ddbdaa8d68699130ca4b9a7b853013aebb3
Shafiq Mohammed <shafiqdms1@gmail.com>
2016-09-02 16:25:24 +0000

    First Ruby on Rails project from scratch to deploy!

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+    
+    def homepage
+        puts "Homepage method executed"
+    end
+    
+    def about
+        puts "About method executed"
+    end
+    
+end
